8b8ee08db9dec420b5d626ec692489ef7afec1d3
NguyenVanPhi92 <83050558+NguyenVanPhi92@users.noreply.github.com>
2022-11-09 16:48:46 +0700

    continute code page detail product

--- doc.md
+++ doc.md
@@ -1,2 +1,2 @@
-LÀM ĐẾN PHÚT 46:32 VIDEO 2
+LÀM ĐẾN PHÚT 55:30 VIDEO 2
 https://www.youtube.com/watch?v=4GMljXR1h78&t=4043s
--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,19 +1,21 @@
 import products from 'assets/data/products'
 import Helmet from 'components/Helmet/Helmet'
 import CommonSection from 'components/UI/CommonSection'
-import React from 'react'
+import React, { useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { Col, Container, Row } from 'reactstrap'
 import 'style/product-detail.scss'
+import { motion } from 'framer-motion'
 
 const ProductDetail = () => {
     const { id } = useParams()
+    const [tab, setTab] = useState('desc')
     const product = products.find((item) => item.id === id)
     const { avgRating, category, description, imgUrl, price, productName, reviews, shortDesc } =
         product
 
     return (
-        <Helmet title="product detai">
+        <Helmet title={productName}>
             <CommonSection />
 
             <section className="pt-0">
@@ -25,7 +27,7 @@ const ProductDetail = () => {
                         <Col lg="6">
                             <div className="product__details">
                                 <h2>{productName}</h2>
-                                <div className="product__rating">
+                                <div className="product__rating d-flex align-content-center gap-5 mb-3">
                                     <div>
                                         <span>
                                             <i className="ri-star-fill"></i>
@@ -47,11 +49,44 @@ const ProductDetail = () => {
                                     <p>({avgRating}ratings)</p>
                                 </div>
 
-                                <span>{price}</span>
+                                <span className="product__price">${price}</span>
                                 <p>{shortDesc}</p>
 
-                                <button className="buy__btn">Add to cart</button>
+                                <motion.button whileTap={{ scale: 1.045 }} className="buy__btn">
+                                    Add to cart
+                                </motion.button>
                             </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </section>
+
+            <section>
+                <Container>
+                    <Row>
+                        <Col lg="12">
+                            <div className="tab__wrapper d-flex align-items-center gap-5">
+                                <h6
+                                    className={`${tab === 'desc' ? 'active__tab' : ''}`}
+                                    onClick={() => setTab('desc')}
+                                >
+                                    Description
+                                </h6>
+                                <h6
+                                    className={`${tab === 'rev' ? 'active__tab' : ''}`}
+                                    onClick={() => setTab('rev')}
+                                >
+                                    Reviews {reviews.length}
+                                </h6>
+                            </div>
+
+                            {tab === 'desc' ? (
+                                <div className="tab__content mt-3">
+                                    <p>{description}</p>
+                                </div>
+                            ) : (
+                                <div>reviews</div>
+                            )}
                         </Col>
                     </Row>
                 </Container>
